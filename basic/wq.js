a5bcf5dc936543cef4178f0efdb092c6afb05cca
acoladog <coladog1112@gmail.com>
2023-07-16 21:10:50 +0800

    1

--- basic/wq.js
+++ basic/wq.js
@@ -1,6 +1,7 @@
 var wfe=document.querySelector("#wfe");
 var wqe=document.querySelector(".wqe");
 var te=0;
+wqe.style.transform="translate3d(0, 0, 0)";
 wfe.onclick=()=>{
     if(te){
         wqe.style.transform="translate3d(-150%, 0, 0)";
