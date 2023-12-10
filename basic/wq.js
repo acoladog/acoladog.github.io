e67f72b99e0efc9a55c054b1e118745f8612163b
acoladog <coladog1112@gmail.com>
2023-12-10 20:28:00 +0800

    123

--- basic/wq.js
+++ basic/wq.js
@@ -2,18 +2,18 @@ var wfe=document.querySelector("#wfe");
 var wqe=document.querySelector(".wqe");
 var te=0;
 wqe.style.transform="translate3d(-150%, 0, 0)";
+document.body.onclick=()=>{
+    if(te){
+        wqe.style.transform="translate3d(-150%, 0, 0)";
+    }
+    te^=1;
+};
 wfe.onclick=()=>{
     if(te){
         wqe.style.transform="translate3d(-150%, 0, 0)";
     }
     else{
         wqe.style.transform="translate3d(0, 0, 0)";
-    }
-    te^=1;
-};
-document.body.onclick=()=>{
-    if(te){
-        wqe.style.transform="translate3d(-150%, 0, 0)";
     }
     te^=1;
 };
