5aff478fed00fba6f92e1b005a6e97043e307953
acoladog <coladog1112@gmail.com>
2023-12-10 20:52:56 +0800

    13

--- basic/wq.js
+++ basic/wq.js
@@ -1,19 +1,22 @@
-var wfe=document.querySelector("#wfe");
-var wqe=document.querySelector(".wqe");
-var te=0;
-wqe.style.transform="translate3d(-150%, 0, 0)";
-document.body.onclick=()=>{
-    if(te){
-        wqe.style.transform="translate3d(-150%, 0, 0)";
+var wfe = document.querySelector("#wfe");
+var wqe = document.querySelector(".wqe");
+var te = 0;
+
+wqe.style.transform = "translate3d(-150%, 0, 0)";
+
+document.body.onclick = () => {
+    if (te) {
+        wqe.style.transform = "translate3d(-150%, 0, 0)";
     }
-    te^=1;
+    te ^= 1;
 };
-wfe.onclick=()=>{
-    if(te){
-        wqe.style.transform="translate3d(-150%, 0, 0)";
+
+wfe.onclick = (event) => {
+    event.stopPropagation(); // 阻止事件冒泡到 document.body
+    if (te) {
+        wqe.style.transform = "translate3d(-150%, 0, 0)";
+    } else {
+        wqe.style.transform = "translate3d(0, 0, 0)";
     }
-    else{
-        wqe.style.transform="translate3d(0, 0, 0)";
-    }
-    te^=1;
+    te ^= 1;
 };
